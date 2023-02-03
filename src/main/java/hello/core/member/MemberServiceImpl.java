4f74a4ff71129aa6af4ead1d01119285d9ff2ae6
박정현 <ros28081@naver.com>
2023-02-03 11:51:23 +0900

    add: OCP, DIP 문제점

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     // 가입하고 회원을 조회하기 위해서는 MemberRepository가 필요
+    // 문제? => MemberRepository는 인터페이스를 의존, 그러나 new MemoryMemberRepository(); 는 구현체를 의존. 추상화/구체화 모두 의존하여 DIP 위반.
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
