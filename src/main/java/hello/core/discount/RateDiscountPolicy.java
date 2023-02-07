766887c2e52b74e46aeaf87e6bb80f65c47b7a77
박정현 <ros28081@naver.com>
2023-02-07 21:16:53 +0900

    feat: 주문 도메인에 정률할인 추가

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,12 @@
+package hello.core.discount;
+
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+    @Override
+    public int discount(Member member, int price) {
+        return 0;
+    }
+}
